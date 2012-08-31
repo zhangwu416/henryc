a554c65f6c768c624c11bb221d200c84b48ffaf4
zhangwu416 <zhangwu416@163.com>
2012-08-31 11:15:10 +0800

    add CMakeLists.txt for myutility

--- test_lib/test_mylog/test.cpp
+++ test_lib/test_mylog/test.cpp
@@ -3,10 +3,15 @@ using namespace std;
 
 #include "log_impl.h"
 #include "log_def.h"
-using namespace henrylib::mylog;
+#include "arg_helper.h"
 
-int main()
+using namespace henrylib::mylog;
+using namespace henrylib::myutility;
+
+int main(int argc, char** argv)
 {
+    arg_helper_t args(argc, argv);
+
     log_impl_t log;
     log.set_path("./log");
     log.set_file_name("test");
