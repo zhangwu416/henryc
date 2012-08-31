7af4eec03a9ba8486d099fdd1173c5a68ab54a01
zhangwu416 <zhangwu416@163.com>
2012-08-31 10:43:21 +0800

    modify misc.h

--- henrylib/myutility/misc.h
+++ henrylib/myutility/misc.h
@@ -6,6 +6,7 @@ namespace henrylib
     namespace myutility
     {
 #define SAFE_DELETE(x) {do{if (x) {delete (x);(x) = NULL;}}while(false);}
+#define SAFE_DELETE_ARR(x) {do{if (x) {delete [](x);(x) = NULL;}}while(false);}
     }
 }
 
